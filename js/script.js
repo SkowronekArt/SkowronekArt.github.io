043c8fd0a04074e3e1a331b6dc3efe0609a31e36
SkowronekArt <anna@skowronek.art.pl>
2017-09-04 23:33:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $.ajax({
 });
 => to jest to samo, co pierwsze trzy linijki kodu
 */
-var tweetLink = "https://twitter.com/intent/tweet?text=";
+var tweeterLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
@@ -27,7 +27,7 @@ function createTweet(input) {
 	if (tweetText.length > 140) {
 	    getQuote();
 	} else {
-	    var tweet = tweetLink + encodeURIComponent(tweetText);
+	    var tweet = tweeterLink + encodeURIComponent(tweetText);
 	    $('.quote').text(quoteText);
 	    $('.author').text("Author: " + quoteAuthor);
 	    $('.tweet').attr('href', tweet);
